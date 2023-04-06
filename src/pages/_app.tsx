08b29434337967a6e1660350dc3e5bc3322cbfcb
Diego Almeida <diegoalmda@gmail.com>
2023-04-06 17:36:23 -0300

    Chore: created home page design

---
+++ src/pages/_app.tsx
@@ -0,0 +1,27 @@
+import { Footer } from '@/components/Footer'
+import { NavBar } from '@/components/NavBar'
+import '@/styles/globals.css'
+import { Quicksand } from 'next/font/google'
+import Head from 'next/head'
+
+const quicksand = Quicksand({
+  subsets: ["latin"],
+  variable: "--font-quick",
+  display: "swap"
+})
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={`${quicksand.variable} font-quick bg-light w-full min-h-screen`}>
+        <NavBar />
+        <Component {...pageProps} />
+        <Footer />
+      </main>
+    </>
+  )
+}
