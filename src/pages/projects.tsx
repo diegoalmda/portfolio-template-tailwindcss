34a79e7aeca78418cb183d8940c93f1209638796
Diego Almeida <diegoalmda@gmail.com>
2023-04-07 18:31:59 -0300

    Chore: created articles page

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -4,8 +4,11 @@ import { Layout } from "@/components/Layout";
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
+import { motion } from "framer-motion";
 
 import project1 from "../../public/images/projects/crypto-screener-cover-image.jpg"
+
+const FramerImage = motion(Image)
 
 const FeaturedProject = ({ type, title, summary, img, link, github }) => {
   return (
@@ -14,7 +17,10 @@ const FeaturedProject = ({ type, title, summary, img, link, github }) => {
       <Link href={link} target="_blank"
         className="w-1/2 cursor-pointer overflow-hidden rounded-lg"
       >
-        <Image src={img} alt={title} className="w-full h-auto" />
+        <FramerImage src={img} alt={title} className="w-full h-auto" 
+          whileHover={{ scale: 1.05 }}
+          transition={{ duration: 0.2 }}
+        />
       </Link>
       <div className="w-1/2 flex flex-col items-start justify-between pl-6">
         <span className="text-primary font-medium text-xl">{type}</span>
@@ -40,7 +46,10 @@ const Project = ({ title, type, img, link, github }) => {
       <Link href={link} target="_blank"
         className="w-full cursor-pointer overflow-hidden rounded-lg"
       >
-        <Image src={img} alt={title} className="w-full h-auto" />
+        <FramerImage src={img} alt={title} className="w-full h-auto" 
+          whileHover={{ scale: 1.05 }}
+          transition={{ duration: 0.2 }}
+        />
       </Link>
       <div className="w-full flex flex-col items-start justify-between mt-4">
         <span className="text-primary font-medium text-xl">{type}</span>
