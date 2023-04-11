1460308671644629c8fa7a975bb400df87b4b7b8
Diego Almeida <diegoalmda@gmail.com>
2023-04-11 18:38:40 -0300

    feat: Created responsive design for articles page

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -12,10 +12,10 @@ const FramerImage = motion(Image)
 
 const FeaturedProject = ({ type, title, summary, img, link, github }) => {
   return (
-    <article className="w-full flex items-center justify-between rounded-3xl border border-solid border-dark shadow-2xl p-12 bg-light relative rounded-br-2xl dark:bg-dark dark:border-light/75">
-      <div className="absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2.5rem] bg-dark rounded-br-3xl dark:bg-light/75" />
+    <article className="w-full flex items-center justify-between rounded-3xl border border-solid border-dark shadow-2xl p-12 bg-light relative rounded-br-2xl dark:bg-dark dark:border-light/75 lg:flex-col lg:p-8 xs:rounded-2xl xs:rounded-br-3xl xs:p-4">
+      <div className="absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2.5rem] bg-dark rounded-br-3xl dark:bg-light/75 xs:-right-2 sm:h-[102%] xs:w-full xs:rounded-[1.5rem]" />
       <Link href={link} target="_blank"
-        className="w-1/2 cursor-pointer overflow-hidden rounded-lg"
+        className="w-1/2 cursor-pointer overflow-hidden rounded-lg lg:w-full"
       >
         <FramerImage src={img} alt={title} className="w-full h-auto" 
           whileHover={{ scale: 1.05 }}
@@ -24,16 +24,16 @@ const FeaturedProject = ({ type, title, summary, img, link, github }) => {
           sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 50vw"
         />
       </Link>
-      <div className="w-1/2 flex flex-col items-start justify-between pl-6">
-        <span className="text-primary font-medium text-xl dark:text-primaryDark">{type}</span>
+      <div className="w-1/2 flex flex-col items-start justify-between pl-6 lg:w-full lg:pl-0 lg:pt-6">
+        <span className="text-primary font-medium text-xl dark:text-primaryDark xs:text-base">{type}</span>
         <Link href={link} target="_blank" className="hover:underline underline-offset-2">
-          <h2 className="my-2 w-full text-left text-4xl font-bold dark:text-light">{title}</h2>
+          <h2 className="my-2 w-full text-left text-4xl font-bold dark:text-light sm:text-sm">{title}</h2>
         </Link>
-        <p className="my-2 font-medium text-dark dark:text-light">{summary}</p>
+        <p className="my-2 font-medium text-dark dark:text-light sm:text-sm">{summary}</p>
         <div className="mt-2 flex items-center">
           <Link href={github} target="_blank" className="w-10"><GithubIcon /></Link>
           <Link href={link} target="_blank"
-            className="ml-4 rounded-lg bg-dark text-light p-2 px-6 text-lg font-smibold dark:bg-light dark:text-dark"
+            className="ml-4 rounded-lg bg-dark text-light p-2 px-6 text-lg font-smibold dark:bg-light dark:text-dark sm:px-4 sm:text-base"
           >Visit Project</Link>
         </div>
       </div>
@@ -43,8 +43,8 @@ const FeaturedProject = ({ type, title, summary, img, link, github }) => {
 
 const Project = ({ title, type, img, link, github }) => {
   return (
-    <article className="w-full flex flex-col items-center justify-center rounded-2xl border border-solid border-dark p-6 bg-light relative dark:bg-dark dark:border-light/75">
-      <div className="absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2rem] bg-dark rounded-br-3xl dark:bg-light/75" />
+    <article className="w-full flex flex-col items-center justify-center rounded-2xl border border-solid border-dark p-6 bg-light relative dark:bg-dark dark:border-light/75 xs:p-4">
+      <div className="absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2rem] bg-dark rounded-br-3xl dark:bg-light/75 md:-right-2 md:w-[101%] xs:h-[102%] xs:rounded-[1.5rem]" />
       <Link href={link} target="_blank"
         className="w-full cursor-pointer overflow-hidden rounded-lg"
       >
@@ -56,15 +56,15 @@ const Project = ({ title, type, img, link, github }) => {
         />
       </Link>
       <div className="w-full flex flex-col items-start justify-between mt-4">
-        <span className="text-primary font-medium text-xl dark:text-primaryDark">{type}</span>
+        <span className="text-primary font-medium text-xl dark:text-primaryDark lg:text-lg md:text-base">{type}</span>
         <Link href={link} target="_blank" className="hover:underline underline-offset-2">
-          <h2 className="my-2 w-full text-left text-3xl font-bold">{title}</h2>
+          <h2 className="my-2 w-full text-left text-3xl font-bold lg:text-2xl">{title}</h2>
         </Link>
         <div className="w-full mt-2 flex items-center justify-between">
           <Link href={link} target="_blank"
-            className="text-lg font-smibold underline"
+            className="text-lg font-smibold underline md:text-base"
           >Visit</Link>
-          <Link href={github} target="_blank" className="w-8"><GithubIcon />{" "}</Link>
+          <Link href={github} target="_blank" className="w-8 md:w-6"><GithubIcon />{" "}</Link>
         </div>
       </div>
     </article>
@@ -80,8 +80,8 @@ export default function Projects() {
       </Head>
       <main className="w-full mb-16 flex flex-col items-center justify-center dark:text-light">
         <Layout className="pt-16">
-          <AnimatedText text="Imagination Trumps Knowledge!" className="mb-16" />
-          <div className="grid grid-cols-12 gap-24 gap-y-32">
+          <AnimatedText text="Imagination Trumps Knowledge!" className="mb-16 lg:!text-7xl sm:mb-8 sm:!text-6xl xs:!text-4xl" />
+          <div className="grid grid-cols-12 gap-24 gap-y-32 xl:gap-x-16 lg:gap-x-8 md:gap-y-24 sm:gap-x-0">
             <div className="col-span-12">
               <FeaturedProject
                 type="Crypto Screener Application" 
@@ -94,7 +94,7 @@ export default function Projects() {
                 github="/"              
               />
             </div>
-            <div className="col-span-6">
+            <div className="col-span-6 sm:col-span-12">
               <Project
                 type="Crypto Screener Application" 
                 title="Featured Project"
@@ -103,7 +103,7 @@ export default function Projects() {
                 github="/"              
               />
             </div>
-            <div className="col-span-6">
+            <div className="col-span-6 sm:col-span-12">
               <Project
                 type="Crypto Screener Application" 
                 title="Featured Project"
@@ -125,7 +125,7 @@ export default function Projects() {
                 github="/"              
               />
             </div>
-            <div className="col-span-6">
+            <div className="col-span-6 sm:col-span-12">
               <Project
                 type="Crypto Screener Application" 
                 title="Featured Project"
@@ -134,7 +134,7 @@ export default function Projects() {
                 github="/"              
               />
             </div>
-            <div className="col-span-6">
+            <div className="col-span-6 sm:col-span-12">
               <Project
                 type="Crypto Screener Application" 
                 title="Featured Project"
